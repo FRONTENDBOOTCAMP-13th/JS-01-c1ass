91d5fe86848ca6e7f077c2a563bb358b386323e7
seuchoi0531 <seuchoi0531@gmail.com>
2025-05-13 22:45:34 +0900

    :white_check_mark: test: dev 모드에서 사용할 테스트 파일 추가 `mac-setting.ts`: dev에 사용할 여러 테스트 코드를 모을 파일 `mac-screen-size.ts`: mac-screen의 크기로 지정할 여러 가지 16:9 비율을 모은 테스트 모듈 파일

---
+++ src/ts/mac-setting.ts
@@ -0,0 +1,37 @@
+import { default as size_arr } from './mac-screen-size.ts';
+const body = document.querySelector('body');
+const mac = document.querySelector('.mac') as HTMLDivElement;
+const mac_screen = document.querySelector('.mac-screen') as HTMLDivElement;
+const mac_bottom = document.querySelector('.mac-bottom') as HTMLDivElement;
+function addSelect() {
+  const select = document.createElement('select');
+  select.classList.add('mac-screen-size');
+  select.style.position = 'absolute';
+  select.style.top = '10px';
+  select.style.left = '10px';
+  select.style.border = '1px solid black';
+  size_arr.forEach((e: number[], i: number) => {
+    const option = document.createElement('option');
+    if (e[0] === 1200) {
+      option.setAttribute('selected', 'true');
+      mac_bottom.style.height = (Math.sqrt(size_arr[i][0]) * 3).toString() + 'px';
+    }
+    const str = `${e[0]}px ${e[1]}px`;
+    option.setAttribute('value', `${i}`);
+    option.textContent = str;
+    select.appendChild(option);
+  });
+  body?.insertBefore(select, mac as HTMLDivElement);
+  select.addEventListener('change', (e: Event) => {
+    const target = e.target as HTMLSelectElement;
+    const selectedIndex = target.value;
+
+    // 문자열 index를 숫자로 변환해야 하는 경우:
+    const index = parseInt(selectedIndex);
+
+    mac_screen.style.width = size_arr[index][0].toString() + 'px';
+    mac_screen.style.height = size_arr[index][1].toString() + 'px';
+    mac_bottom.style.height = (Math.sqrt(size_arr[index][0]) * 3).toString() + 'px';
+  });
+}
+addSelect();
