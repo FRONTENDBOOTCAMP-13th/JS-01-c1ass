4b152259c1e835f496b53d6a7c0148bb746d3382
seuchoi0531 <seuchoi0531@gmail.com>
2025-05-14 13:26:13 +0900

    :white_check_mark: test: icon에 임시로 색을 입히는 테스트 코드 구현

--- src/ts/mac-setting.ts
+++ src/ts/mac-setting.ts
@@ -3,15 +3,15 @@ const body = document.querySelector('body');
 const mac = document.querySelector('.mac') as HTMLDivElement;
 const mac_screen = document.querySelector('.mac-screen') as HTMLDivElement;
 const mac_bottom = document.querySelector('.mac-bottom') as HTMLDivElement;
+const blank_widget_arr = document.querySelectorAll('.blank-widget');
+const icon_bar = document.querySelector('#icon-bar');
+const icon_arr = icon_bar!.querySelectorAll('.icon');
+const icon_color_arr = ['#FFB3BA', '#FFDFBA', '#FFFFBA', '#BAFFC9', '#BAE1FF', '#E3BAFF', '#FFCCE5', '#CCE5FF', '#D5FFCC', '#FFF0BA', '#FFCBA4', '#CBA4FF'];
 
 addSelect();
+paintBlankWidgets();
 addBlankWidgetToggle();
-
-const blank_widget_arr = document.querySelectorAll('.blank-widget');
-Array.from(blank_widget_arr).forEach(e => {
-  (e as HTMLDivElement).style.backgroundColor = 'rgba(255,255,255,0.3)';
-});
-
+paintIconColorful();
 function addSelect() {
   const select = document.createElement('select');
   select.classList.add('mac-screen-size');
@@ -42,6 +42,12 @@ function addSelect() {
   });
 }
 
+function paintBlankWidgets() {
+  Array.from(blank_widget_arr).forEach(e => {
+    (e as HTMLDivElement).style.backgroundColor = 'rgba(255,255,255,0.3)';
+  });
+}
+
 function addBlankWidgetToggle() {
   const btn = document.createElement('button');
   btn.style.position = 'absolute';
@@ -58,8 +64,18 @@ function addBlankWidgetToggle() {
   btn.addEventListener('click', () => {
     console.log('click');
     Array.from(blank_widget_arr).forEach(e => {
-      if ((e as HTMLDivElement).style.backgroundColor) (e as HTMLDivElement).style.backgroundColor = '';
-      else (e as HTMLDivElement).style.backgroundColor = 'rgba(255,255,255,0.3)';
+      if ((e as HTMLDivElement).style.backgroundColor) {
+        (e as HTMLDivElement).style.backgroundColor = '';
+        btn.textContent = '그리드 격자 추가';
+      } else {
+        (e as HTMLDivElement).style.backgroundColor = 'rgba(255,255,255,0.3)';
+        btn.textContent = '그리드 격자 제거';
+      }
     });
+  });
+}
+function paintIconColorful() {
+  Array.from(icon_arr).forEach((e, i) => {
+    (e as HTMLLIElement).style.backgroundColor = icon_color_arr[i];
   });
 }
