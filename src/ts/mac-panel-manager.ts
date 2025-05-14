b24663adc7069d9f6519ec17b613cf36b5766a21
seuchoi0531 <seuchoi0531@gmail.com>
2025-05-14 13:33:42 +0900

    :sparkles: feat: mac-panel 드래그 기능 추가 mac-panel-container 밖으로 나가는 현상 수정할 예정

--- src/ts/mac-panel-manager.ts
+++ src/ts/mac-panel-manager.ts
@@ -44,5 +44,36 @@ function createMacPanel() {
   mac_panel_header_left.appendChild(close_mac_panel);
   mac_panel_header_left.appendChild(minimize_mac_panel);
   mac_panel_header_left.appendChild(full_mac_panel);
+  makeDraggable(mac_panel_content, mac_panel_header);
   return mac_panel_overlay;
+}
+function makeDraggable(modal: HTMLElement, handle: HTMLElement): void {
+  console.log(modal);
+  console.log(handle);
+  let isDragging = false;
+  const containerRec = container?.getBoundingClientRect();
+  let offsetX = 0;
+  let offsetY = 0;
+
+  handle.addEventListener('mousedown', (e: MouseEvent) => {
+    isDragging = true;
+    const rect = modal.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  function onMouseMove(e: MouseEvent) {
+    if (!isDragging) return;
+    modal.style.left = `${e.clientX - offsetX - containerRec!.left}px`;
+    modal.style.top = `${e.clientY - offsetY - containerRec!.top}px`;
+  }
+
+  function onMouseUp() {
+    isDragging = false;
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
 }
