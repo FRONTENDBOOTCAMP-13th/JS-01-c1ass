91b728dc832db364ed0843b94417536975f36eac
seuchoi0531 <seuchoi0531@gmail.com>
2025-05-14 15:58:27 +0900

    :recycle: refactor: icon을 create, insert, remove하는 함수 리팩토링

--- src/ts/icon.ts
+++ src/ts/icon.ts
@@ -25,80 +25,76 @@ class Icon {
     this.isRunning = status;
   }
 }
-// class Iconbar {
-
-// }
+interface Iconbar {
+  icon_list: iconList;
+  icon_cnt: number;
+  createIcon(idx: number, status: number, id?: string): HTMLLIElement;
+  insertIcon(icon: HTMLLIElement): void;
+  removeIcon(icon: Icon): void;
+}
 type iconList = {
   [key: number]: Icon | undefined;
 };
-const icon_list: iconList = {};
-let icon_cnt: number = 0;
-
-function createIcon(idx: number, status: number, id?: string) {
-  const tmpIcon: Icon = new Icon(idx, status);
-
-  icon_list[idx] = tmpIcon;
-  if (id !== undefined) tmpIcon.programID = id;
-  const iconLi = document.createElement('li');
-  iconLi.classList.add('icon');
-  iconLi.dataset.idx = tmpIcon.getIndex().toString();
-  iconLi.dataset.status = tmpIcon.getIsRunning().toString();
-  if (tmpIcon.programID !== undefined) iconLi.dataset.id = tmpIcon.programID.toString();
-  icon_bar!.appendChild(iconLi);
-}
-
-function removeIcon(icon: Icon) {
-  const icon_arr = icon_bar!.querySelectorAll('.icon');
-  console.log(icon);
-  const idx = icon.getIndex();
-  Array.from(icon_arr).forEach(e => {
-    console.log((e as HTMLLIElement).dataset.idx);
-  });
-  const targetIcon = Array.from(icon_arr).find(e => (e as HTMLLIElement).dataset.idx === idx.toString());
-  console.log(targetIcon);
-  icon_list[idx] = undefined;
-  icon_arr[idx].remove();
-  targetIcon?.remove();
-}
+// const icon_list: iconList = {};
+// let icon_cnt: number = 0;
+const iconBar: Iconbar = {
+  icon_list: {},
+  icon_cnt: 0,
+  createIcon(idx: number, status: number, id?: string): HTMLLIElement {
+    const tmpIcon: Icon = new Icon(idx, status);
+    this.icon_list[idx] = tmpIcon;
+    if (id !== undefined) tmpIcon.programID = id;
+    const iconLi = document.createElement('li');
+    iconLi.classList.add('icon');
+    iconLi.dataset.idx = tmpIcon.getIndex().toString();
+    iconLi.dataset.status = tmpIcon.getIsRunning().toString();
+    if (tmpIcon.programID !== undefined) iconLi.dataset.id = tmpIcon.programID.toString();
+    return iconLi;
+  },
+  insertIcon(icon: HTMLLIElement): void {
+    icon_bar!.appendChild(icon);
+  },
+  removeIcon(icon: Icon): void {
+    const icon_arr = icon_bar!.querySelectorAll('.icon');
+    console.log(icon);
+    const idx = icon.getIndex();
+    Array.from(icon_arr).forEach(e => {
+      console.log((e as HTMLLIElement).dataset.idx);
+    });
+    const targetIcon = Array.from(icon_arr).find(e => (e as HTMLLIElement).dataset.idx === idx.toString());
+    console.log(targetIcon);
+    this.icon_list[idx] = undefined;
+    icon_arr[idx].remove();
+    targetIcon?.remove();
+  },
+};
+export { Icon, type Iconbar, type iconList, iconBar };
+// function createIcon(idx: number, status: number, id?: string): HTMLLIElement {
+//   const tmpIcon: Icon = new Icon(idx, status);
+//   icon_list[idx] = tmpIcon;
+//   if (id !== undefined) tmpIcon.programID = id;
+//   const iconLi = document.createElement('li');
+//   iconLi.classList.add('icon');
+//   iconLi.dataset.idx = tmpIcon.getIndex().toString();
+//   iconLi.dataset.status = tmpIcon.getIsRunning().toString();
+//   if (tmpIcon.programID !== undefined) iconLi.dataset.id = tmpIcon.programID.toString();
+//   return iconLi;
+// }
+// function insertIcon(icon: HTMLLIElement) {
+//   icon_bar!.appendChild(icon);
+// }
+// function removeIcon(icon: Icon) {
+//   const icon_arr = icon_bar!.querySelectorAll('.icon');
+//   console.log(icon);
+//   const idx = icon.getIndex();
+//   Array.from(icon_arr).forEach(e => {
+//     console.log((e as HTMLLIElement).dataset.idx);
+//   });
+//   const targetIcon = Array.from(icon_arr).find(e => (e as HTMLLIElement).dataset.idx === idx.toString());
+//   console.log(targetIcon);
+//   icon_list[idx] = undefined;
+//   icon_arr[idx].remove();
+//   targetIcon?.remove();
+// }
 
 // ----------------임시 버튼 테스트 코드---------------------------
-addCreateIconBtn();
-addRemoveIconBtn();
-function addCreateIconBtn() {
-  const body = document.querySelector('body');
-  const btn = document.createElement('button');
-  btn.style.position = 'absolute';
-  btn.style.top = '130px';
-  btn.style.left = '10px';
-  btn.style.border = '1px solid black';
-  btn.style.paddingInline = '4px';
-  btn.style.borderRadius = '8px';
-  btn.style.backgroundColor = '#d9d9d9';
-  btn.setAttribute('type', 'button');
-  btn.textContent = '아이콘 추가';
-
-  body?.insertBefore(btn, body.firstChild);
-  btn.addEventListener('click', () => {
-    createIcon(icon_cnt, 0, icon_cnt.toString());
-    icon_cnt++;
-  });
-}
-function addRemoveIconBtn() {
-  const body = document.querySelector('body');
-  const btn = document.createElement('button');
-  btn.style.position = 'absolute';
-  btn.style.top = '170px';
-  btn.style.left = '10px';
-  btn.style.border = '1px solid black';
-  btn.style.paddingInline = '4px';
-  btn.style.borderRadius = '8px';
-  btn.style.backgroundColor = '#d9d9d9';
-  btn.setAttribute('type', 'button');
-  btn.textContent = '아이콘 삭제';
-
-  body?.insertBefore(btn, body.firstChild);
-  btn.addEventListener('click', () => {
-    removeIcon(icon_list[icon_cnt - 1] as Icon);
-    icon_cnt--;
-  });
-}
