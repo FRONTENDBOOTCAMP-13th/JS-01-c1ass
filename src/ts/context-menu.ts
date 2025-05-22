bfeaa753d814718bdbea8deba8b23354f09cbaeb
seuchoi0531 <seuchoi0531@gmail.com>
2025-05-22 10:46:35 +0900

    :sparkles: feat: context-menu의 클릭이벤트가 동작하도록 mouseup event로 기능 구현

--- src/ts/context-menu.ts
+++ src/ts/context-menu.ts
@@ -1,8 +1,7 @@
+import { iconBar } from './icon-bar.ts';
 const macScreen = document.querySelector('.mac-screen');
 
 macScreen!.addEventListener('contextmenu', e => {
-  console.log(e.currentTarget);
-  console.log(e.target);
   const macScreenMenuArr: { [key: string]: string }[] = [{ changebg: '배경 화면 변경' }, { setusername: '사용자 이름 설정' }];
   const iconMenuArr: { [key: string]: string }[] = [{ open: '열기' }, { delete: '삭제' }];
 
@@ -12,14 +11,14 @@ macScreen!.addEventListener('contextmenu', e => {
 
   e.preventDefault();
   if ((e.target! as HTMLElement).classList.contains('icon-cover')) {
-    iconMenuArr.forEach(e => {
+    iconMenuArr.forEach(el => {
       const ctx_li = document.createElement('li');
       ctx_li.classList.add('context-menu-item');
-      ctx_li.dataset.id = Object.keys(e)[0];
-      ctx_li.textContent = Object.values(e)[0];
+      ctx_li.dataset.id = Object.keys(el)[0];
+      ctx_li.textContent = Object.values(el)[0];
       ctx_ul.appendChild(ctx_li);
+      ctx_ul.dataset.target = `icon${((e.target! as HTMLElement).closest('.icon') as HTMLElement).dataset.id}`;
     });
-    console.log('dddddddd');
     const rect = macScreen?.getBoundingClientRect();
     const ulRect = ctx_ul.getBoundingClientRect();
     ctx_ul.style.left = (e as MouseEvent).clientX - rect!.x + 'px';
@@ -42,4 +41,30 @@ macScreen!.addEventListener('contextmenu', e => {
     document.removeEventListener('mouseup', handleMouseUp);
   };
   document.addEventListener('mouseup', handleMouseUp);
+  ctx_ul.addEventListener('mouseup', e => {
+    if ((e.currentTarget as HTMLElement).dataset.target) {
+      if ((e.currentTarget as HTMLElement).dataset.target!.startsWith('icon')) {
+        const idx = (e.currentTarget as HTMLElement).dataset.target!.slice('icon'.length);
+        if ((e.target as HTMLElement).dataset.id === 'open') {
+          (document.querySelector(`li.icon[data-id="${idx}"]`) as HTMLElement).click();
+        } else if ((e.target as HTMLElement).dataset.id === 'delete') {
+          const targetIcon = document.querySelector(`li.icon[data-id="${idx}"]`) as HTMLElement;
+          targetIcon.style.transition = 'scale';
+          targetIcon.style.transitionDuration = '0.3s';
+          targetIcon.style.scale = '0 1';
+          setTimeout(() => {
+            iconBar.removeIcon(idx);
+          }, 300);
+        }
+      }
+    } else {
+      if ((e.target as HTMLElement).dataset.id === 'changebg') {
+        console.log('changebg click');
+        // 배경 변경
+      } else if ((e.target as HTMLElement).dataset.id === 'setusername') {
+        console.log('setusername click');
+        // 유저 이름 변경
+      }
+    }
+  });
 });
